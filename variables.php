a4fd7621f736c3b62355389c7bc3756c984e6622
NessaAz <vanessa.azenwa@gmail.com>
2022-10-26 13:05:55 +0300

    variables implementation

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables</title>
+</head>
+<body>
+
+    <?php
+    
+    $characterName = "Chimama";
+    $characterBooks = 50;
+
+    echo "there was a woman called $characterName. <br>";
+    echo "she was a prolific writer <br>";
+    echo "$characterName was so inspiring and wrote $characterBooks books <br>";
+    echo "but it is said that they were more than $characterBooks";
+    ?>
+    
+</body>
+</html>
